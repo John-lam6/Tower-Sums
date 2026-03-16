d47da31c48539bbbf48f33f52222aa7e9682a1ea
Johnathan Lam <johnathanlam0406@gmail.com>
2026-03-16 17:00:51 -0400

    Changed to URP pipeline for an outline around blocks. Added outline around blocks. Added blocks that adjust their heights to their values.

--- Assets/Scripts/BlockData.cs
+++ Assets/Scripts/BlockData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BlockState {
     InHotbar,
@@ -9,18 +10,35 @@ public enum BlockState {
 }
 
 public class BlockData : MonoBehaviour {
-    public int value;
+    public int value = 1;
     public BlockState state;
+    public float unitHeight = 1f;
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        UpdateHeight();
+    }
+
+    void OnValidate() {
+        UpdateHeight();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateHeight() {
+        if (value < 1) value = 1;
+
+        Vector3 scale = transform.localScale;
+        scale.y = value * unitHeight;
+        transform.localScale = scale;
+        
+        // keep base grounded so it grows upward and not outward
+        Vector3 pos = transform.position;
+        pos.y = scale.y / 2f;
+        transform.position = pos;
     }
 }
